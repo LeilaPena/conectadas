69cff54198270a244f41c175c134115301a9a16c
Leila Peña <leilajulieta1999@gmail.com>
2023-02-23 12:57:55 -0300

    Reemplace div a nav y elimine div del login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,11 +2,9 @@ import { Layout } from "../../components"
 
 const Login = () => {
     return (
-        <div>
             <Layout>
                 Login
             </Layout>
-        </div>
     )
 }
 
