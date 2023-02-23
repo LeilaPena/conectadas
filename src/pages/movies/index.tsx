9602d2f7d737698ae6ff7a796bad653eb6e3b8f5
Leila Peña <leilajulieta1999@gmail.com>
2023-02-23 18:16:05 -0300

    Traje peliculas con su imagen y titulo

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
 import { Layout } from "../../components"
+import { apiMovies } from "../../utils/axios"
 
 const Movies = () => {
+
+    const [movies, setMovies] = useState<any[]>([])
+
+    useEffect(() => {
+        apiMovies.get('/movie/top_rated').then(response => setMovies(response.data.results))
+    }, [])
+
     return (
         <Layout>
-            Movies
+           {
+                movies.map(movie => (
+                    <div>
+                      <img src={`https://image.tmdb.org/t/p/original`+ movie.poster_path} height={600}/>
+                      <p>{movie.title}</p>
+                    </div>
+                ))
+           }
         </Layout>
     )
 }
