59d34c97e7b1213f8d084a63ed405bfcfad1cfd3
Leila Peña <leilajulieta1999@gmail.com>
2023-02-27 17:08:30 -0300

    Agregue loggeo con token

--- src/hooks/useMe/index.ts
+++ src/hooks/useMe/index.ts
@@ -1,18 +1,45 @@
+import { useEffect } from "react"
+import { tokenGenerator } from "../../helpers/tokenGenerator"
+import { servicesUser } from "../../services/users"
 import {LoginFormType, signUpType} from "../../types"
 
 const useMe = () => {
 
-    const login = ({email, pass}: LoginFormType) => {
+    useEffect(() => {
+        loginWithToken()
+    }, [])
 
-    }
+    const login = async ({email, pass}: LoginFormType) => {
+
+        const user = await servicesUser.getBy(email, "email")
+
+        if (user?.password === pass) {
+            
+            const token = tokenGenerator()
+            
+            servicesUser.update({sessionToken: token, id: user.id})
+
+            localStorage.setItem('token', token)
+        
+
+        } else {
+            console.log('login incorrecto')
+        }
+     }
 
     const signup = (user: signUpType) => {
 
 
     }
 
-    const loginWithToken = () => {
+    const loginWithToken = async () => {
+        const token = localStorage.getItem('token')
 
+        if(token){
+            const user = await servicesUser.getBy(token, "sessionToken")
+            console.log(user)
+        }
+       
     }
 
     const logout = () => {
