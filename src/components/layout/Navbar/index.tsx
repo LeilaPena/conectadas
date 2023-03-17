8ca8e1f86c5dc285735082e908a20989ccec3e9b
Leila Peña <leilajulieta1999@gmail.com>
2023-03-17 17:57:29 -0300

    Hice desaparecer accesos a ciertas pages depende si estas loggeado o no

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -7,31 +7,45 @@ import { House, Film, People, BoxArrowLeft, PersonPlus, BoxArrowRight } from "re
 
 const Navbar = () => {
 
-  const {logout} = useMe()
+  const {logout, me} = useMe()
+
   return (
     <>
       <NavbarRB bg="dark" variant="dark">
         <Container fluid>
           <NavbarRB.Brand>Conectadas</NavbarRB.Brand>
           <Nav className="ms-auto justify-content-end">
+          {me && (
+            <>
             <NavLink className="nav-link mx-2" to="/">
               <House size={25}/>
             </NavLink>
+
             <NavLink className="nav-link mx-2" to="/movies">
               <Film size={23}/>
             </NavLink>
+
             <NavLink className="nav-link mx-2" to="/users">
               <People size={25}/>
             </NavLink>
+      
+            <Nav.Link className="nav-link mx-2" onClick={logout}>
+              <BoxArrowRight size={25}/>
+            </Nav.Link>
+            </>
+            )}
+
+            {!me && (
+            <>
             <NavLink className="nav-link mx-2" to="/login">
               <BoxArrowLeft size={25}/>
             </NavLink>
             <NavLink className="nav-link mx-2" to="/signup">
               <PersonPlus size={25}/>
             </NavLink>
-            <Nav.Link className="nav-link mx-2" onClick={logout}>
-              <BoxArrowRight size={25}/>
-            </Nav.Link>
+            </>
+            )}
+            
             <p>Leila</p>
           </Nav>
         </Container>
