4f83a76d67f40ea8850c3fad092e1c53ef2a8fbb
Leila Peña <leilajulieta1999@gmail.com>
2023-03-20 12:09:16 -0300

    Agregue ValidationMsgs

---
+++ src/components/forms/validationsMsgs.ts
@@ -0,0 +1,5 @@
+export const validationMsgs = {
+    required: "Este campo es necesario.",
+    email: "Debe ingresar un email",
+    pass: "La contraseña debe tener 8 caracteres, uno especial, una mayuscula y un número", 
+}
