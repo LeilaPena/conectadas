7e9eb29471151a43ac29ec9af8ca3476e775a346
Leila Peña <leilajulieta1999@gmail.com>
2023-03-28 22:32:27 -0300

    Hice que los post se guarden en firebase

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,23 +5,23 @@ export type User = {
   country: string;
   email: string;
   friends: string[];
-  id: string;
-  lastname: string;
-  name: string;
+  id: string | undefined;
+  lastname: string | undefined;
+  name: string | undefined;
   pass: string;
   sessionToken: string | null;
   status: string;
 };
 
 export type Post = {
-  id: string
-  user: Pick<User, "id" | "name" | "lastname">
-  image?: string
-  title: string
-  detail: string
-  comments: Comment[]
-  date: Date
+  id: string;
+  user: Pick<User, "id" | "name" | "lastname">;
+  image?: string;
+  title: string;
+  detail: string;
+  date: Date;
 }
+export type postFormType = Omit<Post, 'id'>
 
 export type signUpType = Omit<User, "id">;
 
