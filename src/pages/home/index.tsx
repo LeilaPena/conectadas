c0fd4a59aa54b7ca9a4d5aa512093e38f773796d
Leila Peña <leilajulieta1999@gmail.com>
2023-03-29 20:13:52 -0300

    Agregue services de getAll, empece a traer posts

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,16 +3,22 @@ import { withAuth } from "../../hoc"
 import { FormPost } from "../../components"
 import { useMe } from "../../hooks"
 import { servicesPosts } from "../../services/posts"
+import { useEffect, useState } from "react"
+import { Post } from "../../types"
 
 const HomePage = () => {
 
     const {me} = useMe();
+    const [post, setPost] = useState<Post[]>([])
 
     const addPosts = (data: {title: string, detail: string }) => {
         const user = {id: me?.id, name: me?.name, lastname: me?.lastname}
         me && servicesPosts.add({title: data.title, detail: data.detail, date: new Date(), user})
     }
 
+    useEffect(() => {
+        servicesPosts.getAll().then(data => setPost(data))
+    }, [])
 
     return (
         <Layout>
