27c68e0a451fd516dfbd1c8cc0820e0bb99c7a08
Leila Peña <leilajulieta1999@gmail.com>
2023-03-30 17:45:50 -0300

    Hice funcionalidad para que se vean los posts

---
+++ src/hooks/usePosts/index.ts
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react"
+import { StoreContext } from "../../contexts"
+import { servicesPosts } from "../../services/posts"
+import { useMe } from "../useMe"
+
+const usePosts = () => {
+
+    const { posts, loadPosts } = useContext(StoreContext)
+    const { me } = useMe();
+
+    useEffect(() => {
+        !posts.length && getPosts();
+    }, [posts])
+
+    const getPosts = async () => {
+        const response = await servicesPosts.getAll();
+        loadPosts(response);
+    }
+
+    const addPosts = async (data: { title: string, detail: string }) => {
+        const user = { id: me?.id, name: me?.name, lastname: me?.lastname }
+        await servicesPosts.add({ title: data.title, detail: data.detail, date: new Date(), user })
+        getPosts()
+    }
+
+    return { posts, addPosts }
+}
+
+export { usePosts }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,28 @@
 import { Layout, Post } from "../../components"
 import { withAuth } from "../../hoc"
 import { FormPost } from "../../components"
-import { useMe } from "../../hooks"
-import { servicesPosts } from "../../services/posts"
-import { useEffect, useState } from "react"
-import { Post } from "../../types"
+import { usePosts } from "../../hooks"
+
+import { Container } from "react-bootstrap"
 
 const HomePage = () => {
 
-    const {me} = useMe();
-    const [post, setPost] = useState<Post[]>([])
-
-    const addPosts = (data: {title: string, detail: string }) => {
-        const user = {id: me?.id, name: me?.name, lastname: me?.lastname}
-        me && servicesPosts.add({title: data.title, detail: data.detail, date: new Date(), user})
-    }
-
-    useEffect(() => {
-        servicesPosts.getAll().then(data => setPost(data))
-    }, [])
+    const { posts, addPosts } = usePosts()
 
     return (
         <Layout>
+            <Container fluid>
             <FormPost onSubmit={addPosts}/>
+            
+            {
+                posts.map(post => (
+                    <div className="m-4" key={post.id}>
+                        <Post title={post.title} date={post.date} detail={post.detail} user={post.user} id={post.id}/>
+                    </div>
+                ))
+            }
+            </ Container>
+
         </Layout>
     )
 }
